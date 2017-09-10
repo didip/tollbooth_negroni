1737e67db822ad834b1a5fa16ac6c8af8d46e84f
Didip Kerabat <didipk@gmail.com>
2017-09-09 21:09:03 -0700

    Update to conform with v2.x.x API.

--- tollbooth_negroni.go
+++ tollbooth_negroni.go
@@ -1,20 +1,17 @@
 package tollbooth_negroni
 
 import (
-	"github.com/urfave/negroni"
 	"github.com/didip/tollbooth"
-	"github.com/didip/tollbooth/config"
+	"github.com/didip/tollbooth/limiter"
+	"github.com/urfave/negroni"
 	"net/http"
 )
 
-func LimitHandler(limiter *config.Limiter) negroni.HandlerFunc {
+func LimitHandler(lmt *limiter.Limiter) negroni.HandlerFunc {
 	return negroni.HandlerFunc(func(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
-		httpError := tollbooth.LimitByRequest(limiter, r)
+		httpError := tollbooth.LimitByRequest(lmt, r)
 		if httpError != nil {
-			w.Header().Add("Content-Type", limiter.MessageContentType)
-			/* RHMOD Fix for error "http: multiple response.WriteHeader calls"
-			     Reverse the sequence of the functions calls w.WriteHeader() and w.Write()
-			*/
+			w.Header().Add("Content-Type", lmt.GetMessageContentType())
 			w.WriteHeader(httpError.StatusCode)
 			w.Write([]byte(httpError.Message))
 			return
