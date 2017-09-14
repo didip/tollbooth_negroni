be41abcb11bb944676364abdfa601ed6825674ab
Didip Kerabat <didipk@gmail.com>
2017-09-14 09:14:37 -0700

    Upgrade to Tollbooth 2.2.0

--- tollbooth_negroni.go
+++ tollbooth_negroni.go
@@ -9,7 +9,7 @@ import (
 
 func LimitHandler(lmt *limiter.Limiter) negroni.HandlerFunc {
 	return negroni.HandlerFunc(func(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
-		httpError := tollbooth.LimitByRequest(lmt, r)
+		httpError := tollbooth.LimitByRequest(lmt, w, r)
 		if httpError != nil {
 			w.Header().Add("Content-Type", lmt.GetMessageContentType())
 			w.WriteHeader(httpError.StatusCode)
